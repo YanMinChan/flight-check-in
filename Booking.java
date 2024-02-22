af00112d3a44a789e87cc69d022e498182f67a4f
shon.kel <ssk2011@hw.ac.uk>
2024-02-22 16:19:58 +0000

    added booking class instance variables

---
+++ Booking.java
@@ -0,0 +1,9 @@
+public class Booking {
+    private String bookingReference;
+	private String passengerName;
+	private String flightCode;
+	private boolean checkIn;
+	private Baggage baggage;
+	
+
+}
