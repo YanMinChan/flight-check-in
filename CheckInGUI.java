c0d5c1ffffe8f8dbce29eb02ccf5f353a8d404c7
Dhanush 98 <dhanushk1098@gmail.com>
2024-02-23 13:57:24 +0000

    GUI code added

---
+++ CheckInGUI.java
@@ -0,0 +1,64 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+
+
+public class GUI implements ActionListener
+{
+	//GUI for the flight booking system
+	
+	JFrame frame;
+	JButton CheckInButton;
+	JButton filterButton;
+	JPanel panel;
+	private JTextArea HeadingArea;
+	
+	
+	 public GUI() 
+	 {
+	        frame = new JFrame();
+	        panel = new JPanel();
+	        HeadingArea = new JTextArea("Check - In"); //Heading text
+	        HeadingArea.setEditable(false);
+	        
+	        CheckInButton = new JButton("Check In ");  //button for checking in
+	        
+	        CheckInButton.addActionListener(this);
+	        
+	        //placement of heading
+	        panel.setLayout(new BorderLayout());
+	        panel.add(HeadingArea, BorderLayout.NORTH); 
+	        panel.add(new JScrollPane(HeadingArea), BorderLayout.CENTER); // Add a JScrollPane for the HeadingArea
+	        
+	        //placement for check in Button
+	        panel.add(CheckInButton, BorderLayout.SOUTH); 
+	        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+	        
+	        frame.add(panel);
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        
+	        
+	        frame.pack();
+	        frame.setVisible(true);
+	 }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// tbd
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		new GUI();
+	}
+
+}
