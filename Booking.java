307539a297e836c5ea9ac36b196988fb7f358f87
shon.kel <ssk2011@hw.ac.uk>
2024-02-27 11:44:20 +0000

    booking class completed

--- Booking.java
+++ Booking.java
@@ -11,6 +11,41 @@ public class Booking {
 		this.flightCode = flightCode;
 		this.checkIn = checkIn;
 	}
+	public String getbookingref(){
+		return bookingReference;
+	}
+	public String getpassengerName(){
+		return passengerName;
+	} 
+	public String getflightCode(){
+		return flightCode;
+	}
+	public boolean getcheckIn(){
+		return checkIn;
+	}
+	public Baggage getBaggage(){
+		return baggage;
+	}
+	public void setbookingref(String b){
+		bookingReference=b;
+	}
+	public void setpassengerName(String p){
+		passengerName=p;
+	}
+	public void setflightCode(String f){
+		flightCode=f;
+	}
+	public void setcheckIn(boolean c){
+		checkIn=c;
+	}
+	public void setbaggage(Baggage ba){
+		baggage = ba;
+	}
+	public String getPassengerLastName(){
+		String[] Name = passengerName.split("\\s+");
+		return Name[Name.length-1];
+	}
+
 	
 	
 
