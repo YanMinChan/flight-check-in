7dc098d0beda428c764d79a2224715fef5e74ce2
Yan Min <yc89@hw.ac.uk>
2024-02-29 20:12:17 +0000

    Added name and booking ref textfield

--- CheckInGUI.java
+++ CheckInGUI.java
@@ -1,51 +1,61 @@
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class CheckInGUI implements ActionListener
 {
 	//GUI for the flight booking system
-	
-	JFrame frame;
-	JButton CheckInButton;
-	JButton filterButton;
-	JPanel panel;
-	private JTextArea HeadingArea;
+	private JFrame frame;
+    private JTextField nameField, bookingRefField;
+	private JButton checkInButton;
+	private JPanel centerPanel;
+	private JLabel headingArea;
 	
 	
 	public CheckInGUI() {
-		// Initialising JFrame and JPanel
+		
+        // .. Component setup
         frame = new JFrame();
-        panel = new JPanel();
-        
-        HeadingArea = new JTextArea("Check - In"); //Heading text
-        HeadingArea.setEditable(false);
-        
-        // Check in action
-        CheckInButton = new JButton("Check In ");  //button for checking in
-        CheckInButton.addActionListener(this);
-        
-        //placement of heading
-        panel.setLayout(new BorderLayout());
-        panel.add(HeadingArea, BorderLayout.NORTH); 
-        panel.add(new JScrollPane(HeadingArea), BorderLayout.CENTER); // Add a JScrollPane for the HeadingArea
-        
-        //placement for check in Button
-        panel.add(CheckInButton, BorderLayout.SOUTH); 
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        
-        frame.add(panel);
+        centerPanel = new JPanel();
+        nameField = new JTextField(15);
+        bookingRefField = new JTextField(15);
+        checkInButton = new JButton("Check In ");  //button for checking in
+        headingArea = new JLabel("Check - In"); //Heading text
+
+        // ..  Layout setup
+        // Set Up JFrame
+        frame.setTitle("Check-In Kiosk");
+        frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Heading
+        headingArea.setFont(new Font("Arial", Font.BOLD, 24));
+        headingArea.setHorizontalAlignment(JLabel.CENTER);
+
+        // Center
+        centerPanel.setLayout(new GridLayout(2, 2, 10, 10));
+        centerPanel.add(new JLabel("Last name:"));
+        centerPanel.add(nameField);
+        centerPanel.add(new JLabel("Booking reference:"));
+        centerPanel.add(bookingRefField);
+
+        // .. Panel setup
+        frame.add(headingArea, BorderLayout.NORTH);
+        frame.add(centerPanel, BorderLayout.CENTER);
+        frame.add(checkInButton, BorderLayout.SOUTH);
+
+        // -- Action listener
+        checkInButton.addActionListener(this);
         
+        // //placement of heading
+        // centerPanel.setLayout(new BorderLayout());
+        // centerPanel.add(HeadingArea, BorderLayout.NORTH); 
+        // centerPanel.add(new JScrollPane(HeadingArea), BorderLayout.CENTER); // Add a JScrollPane for the HeadingArea
+        
+        // //placement for check in Button
+        // centerPanel.add(checkInButton, BorderLayout.SOUTH); 
+        // centerPanel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+ 
         frame.pack();
         frame.setVisible(true);
 	 }
@@ -58,7 +68,7 @@ public class CheckInGUI implements ActionListener
 	
 	public static void main(String[] args)
 	{
-		CheckInGUI gui = new CheckInGUI();
+		new CheckInGUI();
 	}
 
 }
