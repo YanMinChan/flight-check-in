e85c73021d567892ba570eb0f59142fb565a6200
Yan Min Chan <yc89@hw.ac.uk>
2024-03-02 13:39:11 +0000

    Added manager class

---
+++ CheckInManager.java
@@ -0,0 +1,22 @@
+public class CheckInManager {
+
+	public CheckInManager(){}
+
+	public void run(){
+		//Initialize check in system and read files
+		CheckInSystem sys = new CheckInSystem();
+		sys.readFile("bookings.txt", "Booking");
+		sys.readFile("flights.txt", "Flight");
+
+		//Check if readfile works for flight.txt
+		String carrier = sys.getFlightMap().get("LAAA002").getCarrier();
+		System.out.println(carrier);
+
+		//Run GUI
+		new CheckInGUI();
+	}
+
+	// public static void main(String[] args){
+	// 	new CheckInManager().run();
+	// }
+}
