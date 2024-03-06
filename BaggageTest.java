e14ce70a68ccaa8079671b93c8625ed6100e17f6
shon.kel <ssk2011@hw.ac.uk>
2024-03-06 17:44:39 +0000

    comments BaggageTest

--- BaggageTest.java
+++ BaggageTest.java
@@ -5,48 +5,48 @@ public class BaggageTest {
 
     @Before
     public void setUp() {
-        baggage = new Baggage(10, 25, 60); // Sample values for dimension, weight, and fee
+        baggage = new Baggage(10, 25, 60); // values for dimension, weight, and fee
     }
 
     @Test
-    public void testgetDim() {
+    public void testgetDim() { //testing that checks getDim() method
         assertEquals(10, baggage.getDim(),0.001);
     }
 
     @Test
-    public void testgetWeight() {
+    public void testgetWeight() { //test that checks for getWeight method
         assertEquals(25, baggage.getWeight(),0.0001);
     }
 
     @Test
-    public void testgetFee() {
-        assertEquals(60, baggage.getFee(), 0.001);
+    public void testgetFee() {//test that checks for getFee() method
+        assertEquals(60, baggage.getFee(), 0.001);//testing for getFee() method
     }
 
     @Test
-    public void testsetDim() {
+    public void testsetDim() { //test that checks setDim() method
         baggage.setDim(15);
-        assertEquals(15, baggage.getDim(), 0.005);
+        assertEquals(15, baggage.getDim(), 0.005); 
     }
     @Test
-    public void testsetFee() {
+    public void testsetFee() {  //test that checks for setFee() method
         baggage.setFee(60);
-        assertEquals(60, baggage.getFee(), 0.01);
+        assertEquals(60, baggage.getFee(), 0.01); 
     }
     @Test
     public void testSetWeight() throws IllegalBaggageWeightException {
         
 
-        // Test setting a valid weight
-        baggage.setWeight(100);
-        assertEquals(100, baggage.getWeight(),0.001);
+        //test setting a valid weight
+        baggage.setWeight(80);
+        assertEquals(80, baggage.getWeight(),0.001);
 
-        // Test setting a weight that is too high
+        //test setting a weight that is too high
         assertThrows(IllegalBaggageWeightException.class, () -> {
             baggage.setWeight(201);
         });
 
-        // Test setting a weight that is too low
+        //test setting a weight that is too low
         assertThrows(IllegalBaggageWeightException.class, () -> {
             baggage.setWeight(-1);
         });
