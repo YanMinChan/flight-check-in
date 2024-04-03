695a2ce97306f3f884823917d317df8cfc3bdf7b
shon.kel <ssk2011@hw.ac.uk>
2024-04-03 02:39:12 +0100

    flightdisplayclass

---
+++ src/FlightDisplay.java
@@ -0,0 +1,30 @@
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class FlightDisplay extends JPanel{
+    private FlightSharedQueue s;
+    public void update() {
+
+		removeAll();
+		
+	
+		String details = " ";
+		JLabel label = new JLabel(details);
+		add(label);
+		
+		
+		for (Flight f: s.getQueue()) {
+			details = String.format("%-10s %-30s %-10s %-10s", f.getFlightCode(), f.getDest());
+			label = new JLabel(details);
+			add(label);
+		}
+		
+        revalidate();
+        repaint();
+		
+//		if (queueDetails.length() > 0) {
+//			this.add(new JLabel(queueDetails.toString()));
+//
+//      }  
+	}
+}
